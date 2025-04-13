ed5ab50ff2bff5af111049b309152d23cfaeb273
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-04-12 23:31:27 -0600

    channel semantics

--- go_chan/go_chan.go
+++ go_chan/go_chan.go
@@ -15,4 +15,26 @@ func main(){
         }(i)
     }
     time.Sleep(10 * time.Millisecond)
+    ch := make(chan string) 
+    go func() {
+        for i := 0; i < 3; i++{
+            msg := fmt.Sprintf("message #%d", i+1)
+            ch <- msg
+        } 
+        close(ch)
+    }()
+    for msg := range ch {
+        fmt.Println("got:", msg)
+    }
+    msg, ok := <- ch // ch is closed
+    fmt.Printf("closed: %#v (ok=%v)\n", msg, ok)
+
+
+    /* channel semantics
+    - send & receive will block until opposite operation (*)
+    - receive from a closed channel will return the zero value without blocking 
+    - send to a closed channel will panic
+    - closing a closed channel will panic
+    - send/receive to a nil channel will block forever
+    */
 }

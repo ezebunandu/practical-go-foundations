5ed72298a6338ed2a7083b3458a7890ce8dce59e
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-04-15 07:16:27 -0600

    site_time: using sync.Wg

---
+++ site_time/go.mod
@@ -0,0 +1,3 @@
+module site_time
+
+go 1.24.2
---
+++ site_time/site_time.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"sync"
+	"time"
+)
+
+func siteTime(url string) {
+    start := time.Now()
+
+    resp, err := http.Get(url)
+    if err != nil {
+        log.Printf("ERROR: %s -> %s", url, err)
+        return
+    }
+
+    defer resp.Body.Close()
+    if _, err := io.Copy(io.Discard, resp.Body); err != nil {
+        log.Printf("ERROR: %s -> %s", url, err)
+    }
+
+    duration := time.Since(start)
+    log.Printf("INFO: %s -> %v", url, duration)
+}
+
+func main(){
+    urls := []string{
+        "https://google.com",
+        "https://apple.com",
+        "https://dumb.site",
+    }
+
+    var wg sync.WaitGroup
+    // wg.Add(len(urls))
+    for _, url := range urls {
+        wg.Add(1)
+        url := url
+        go func(){
+            defer wg.Done()
+            siteTime(url)
+        }()
+    }
+    wg.Wait()
+}

bf3ffe441c79b320e08861f103ada04513904cd9
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-05-06 23:04:18 -0600

    github: use encoding/json to decode response

--- github/github.go
+++ github/github.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 )
 
 // Given a github user login, return name and number of public repos
 func main() {
+    fmt.Println(UserInfo("ezebunandu"))
+}
+func demo () {
     resp, err := http.Get("https://api.github.com/users/ardanlabs")
     if err != nil {
         fmt.Println("ERROR:", err)
@@ -16,5 +21,43 @@ func main() {
         fmt.Printf("ERROR: bad status - %s\n", resp.Status)
         return
     }
-    
+    ctype := resp.Header.Get("Content-Type") 
+    fmt.Println("content-type:", ctype)
+    var reply struct {
+        Name string `json:"name"`
+        PublicRepos int `json:"public_repos"`
+    }
+    dec := json.NewDecoder(resp.Body)
+    if err := dec.Decode(&reply); err != nil {
+        fmt.Println("ERROR:", err)
+        return
+    }
+    // io.Copy(os.Stdout, resp.Body)
+    fmt.Println(reply.Name, reply.PublicRepos)
+}
+
+func UserInfo(user string) (string, int, error){
+    url := "https://api.github.com/users/" + user
+    resp, err := http.Get(url)
+    if err != nil {
+        return "", 0, err
+    }
+    defer resp.Body.Close()
+    if resp.StatusCode != http.StatusOK {
+        return "", 0, fmt.Errorf("%q - bad status: %s", url, resp.Status)
+    }
+    return parseResponse(resp.Body)
+}
+
+func parseResponse(r io.Reader) (string, int, error) {
+    var reply struct {
+        Name string `json:"name"`
+        PublicRepos int `json:"public_repos"`
+    }
+    dec := json.NewDecoder(r)
+    if err := dec.Decode(&reply); err != nil {
+        return "", 0, err
+    }
+    // io.Copy(os.Stdout, resp.Body)
+    return reply.Name, reply.PublicRepos, nil
 }

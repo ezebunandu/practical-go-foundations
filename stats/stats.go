d2c37226248e572950ac23f8cf1fb973097ce76f
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-05-12 22:40:00 -0600

    generics: basic usage

---
+++ stats/stats.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println(Relu(7))
+    fmt.Println(Relu(-1))
+    fmt.Println(Relu(1.3))
+    fmt.Println(Relu(-0.25))
+
+}
+
+/* func ReluInt(i int) int {
+    if i < 0 {
+        return 0
+    }
+    return i
+}
+
+func ReluFloat64(i float64) float64 {
+    if i < 0 {
+        return 0
+    }
+    return i
+} */
+
+// T is a "type constraint"
+func Relu[T int | float64] (i T) T {
+    if i < 0 {
+        return 0
+    }
+    return i
+}

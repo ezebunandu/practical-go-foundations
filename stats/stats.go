963b40b77dd299d0f0d61e9edc4d4827844d56ad
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-05-13 21:01:19 -0600

    generics: generic data structures

--- stats/stats.go
+++ stats/stats.go
@@ -11,6 +11,12 @@ func main() {
     fmt.Println(Relu(1.3))
     fmt.Println(Relu(time.February))
 
+    m, err := NewMatrix[float64](10, 3)
+    if err != nil {
+        fmt.Println("ERROR:", err)
+    }
+    fmt.Println("m:", m)
+    fmt.Println(m.At(3, 2))
 }
 
 /* func ReluInt(i int) int {
@@ -27,6 +33,28 @@ func ReluFloat64(i float64) float64 {
     return i
 } */
 
+type Matrix [T Number] struct {
+    Rows int
+    Cols int
+    data []T
+}
+
+func NewMatrix[T Number] (rows, cols int) (*Matrix[T], error) {
+    if rows <= 0 || cols <= 0 {
+        return nil, fmt.Errorf("bad dimensions: %d/%d", rows, cols)
+    }
+    m := Matrix[T] {
+        Rows: rows,
+        Cols: cols,
+        data: make([]T, rows * cols),
+    }
+    return &m, nil
+}
+
+func (m *Matrix[T]) At (row, col int) T {
+    i := (row * m.Cols) + col
+    return m.data[i]
+}
 type Number interface {
     ~int | ~float64
 }

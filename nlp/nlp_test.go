7ec2eac2b4e17fd85d0ef23a8c4303d028b8e4be
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-06-02 23:00:31 -0600

    testing: table tests

--- nlp/nlp_test.go
+++ nlp/nlp_test.go
@@ -15,3 +15,22 @@ func TestTokenize(t *testing.T) {
         t.Fatalf("expected %#v, got %#v", expected, tokens)
     }
 }
+
+func TestTokenizeTable(t *testing.T){
+    var cases = []struct{
+        text string
+        tokens []string
+    }{
+        {"Who's on first?", []string{"who", "s", "on", "first"},},
+        {"A lil bit of project engineering", []string{"a", "lil", "bit", "of", "project", "engineering"},},
+        {"", nil},
+    }
+    for _, tc := range cases {
+        t.Run(tc.text, func(t *testing.T) {
+            tokens := Tokenize(tc.text)
+            if !slices.Equal(tc.tokens, tokens) {
+                t.Fatalf("expected %#v, got %#v", tc.tokens, tokens)
+            }
+        })
+    }
+}

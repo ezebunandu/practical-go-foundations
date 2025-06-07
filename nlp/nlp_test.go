7aa8c72f172eeac1304f6672eb1b73663bd2c348
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2025-06-06 22:22:02 -0600

    testing: loading test cases from yaml

--- nlp/nlp_test.go
+++ nlp/nlp_test.go
@@ -4,42 +4,41 @@ import (
 	"os"
 	"testing"
 
-    "github.com/stretchr/testify/require"
+	"github.com/BurntSushi/toml"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestTokenize(t *testing.T) {
-    // setup : call a function
-    // teardown: defer/t.Cleanup
-    text := "A lil bit of project engineering"
-    tokens := Tokenize(text)
-    expected := []string{"a", "lil", "bit", "of", "project", "engineering"}
+	// setup : call a function
+	// teardown: defer/t.Cleanup
+	text := "A lil bit of project engineering"
+	tokens := Tokenize(text)
+	expected := []string{"a", "lil", "bit", "of", "project", "engineering"}
 
-    require.Equal(t, expected, tokens)
-    // Before testify
-/*     if !slices.Equal(tokens, expected){
-        t.Fatalf("expected %#v, got %#v", expected, tokens)
-    } */
+	require.Equal(t, expected, tokens)
+	// Before testify
+	/*     if !slices.Equal(tokens, expected){
+	       t.Fatalf("expected %#v, got %#v", expected, tokens)
+	   } */
 }
 
-func TestTokenizeTable(t *testing.T){
-    var cases = []struct{
-        text string
-        tokens []string
-    }{
-        {"Who's on first?", []string{"who", "s", "on", "first"},},
-        {"A lil bit of project engineering", []string{"a", "lil", "bit", "of", "project", "engineering"},},
-        {"", nil},
-    }
-    for _, tc := range cases {
-        t.Run(tc.text, func(t *testing.T) {
-            tokens := Tokenize(tc.text)
-            require.Equal(t, tc.tokens, tokens)
-            // Before testify
-            /* if !slices.Equal(tc.tokens, tokens) {
-                t.Fatalf("expected %#v, got %#v", tc.tokens, tokens)
-            } */
-        })
-    }
+func TestTokenizeTable(t *testing.T) {
+	cases := loadTokenizeCase(t)
+	for _, tc := range cases {
+		name := tc.Name
+		if name == "" {
+			name = tc.Text
+		}
+		t.Run(name, func(t *testing.T) {
+			tokens := Tokenize(tc.Text)
+            if tokens == nil {
+                tokens = []string{}
+            }
+			t.Logf("Text: %q, Got: %#v, Expected: %#v", tc.Text, tokens, tc.Tokens)
+			require.Equal(t, tc.Tokens, tokens)
+		})
+	}
 }
 
 /*
@@ -51,8 +50,29 @@ Selecting tests:
 
 var inCI = os.Getenv("CI") != ""
 
-func TestInCI(t *testing.T){
-    if !inCI {
-        t.Skip("not in CI")
-    }
+func TestInCI(t *testing.T) {
+	if !inCI {
+		t.Skip("not in CI")
+	}
+}
+
+type tokCase struct {
+	Text   string
+	Tokens []string
+	Name   string
+}
+
+func loadTokenizeCase(t *testing.T) []tokCase {
+	file, err := os.Open("testdata/tokenize_cases.toml")
+	require.NoError(t, err)
+	defer file.Close()
+
+	var data struct {
+		Cases []tokCase `toml:"cases"`
+	}
+	dec := toml.NewDecoder(file)
+	_, err = dec.Decode(&data)
+	require.NoError(t, err)
+	t.Logf("Loaded cases: %+v", data.Cases)
+	return data.Cases
 }
